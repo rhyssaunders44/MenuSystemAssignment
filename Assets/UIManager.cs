5908dab5250df24a132469d043a77054963ba6ae
rhyssaunders44 <78834755+rhyssaunders44@users.noreply.github.com>
2021-05-19 10:35:54 +1000

    stuff

---
+++ Assets/UIManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public void AnyKeyScreen()
+    {
+
+    }
+
+    public void FullScreen()
+    {
+
+    }
+
+    public void Pause()
+    {
+        
+    }
+
+    public void ToMainMenu()
+    {
+
+    }
+
+    public void Save()
+    {
+
+    }
+
+    public void LoadLevel()
+    {
+
+    }
+
+    public void Mute()
+    {
+
+    }
+
+    public void PlayGame()
+    {
+
+    }
+
+    public void Options()
+    {
+
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    void ElementFade()
+    {
+
+    }
+}
